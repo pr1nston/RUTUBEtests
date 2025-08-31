ee9ba64aa1d113c401c0b4b6317e16fc3fc19d6a
Mark Shalabin <prinstondota2@gmail.com>
2025-08-31 17:41:20 +0300

    sa

--- Rutube_tests/conftest.py
+++ Rutube_tests/conftest.py
@@ -7,7 +7,7 @@ from playwright.sync_api import TimeoutError
 @pytest.fixture(scope="session")
 def browser_instance():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
 
